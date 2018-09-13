fdd6673dd3c7dc4ae8ce41e6be4a38dd098b92f6
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-09-13 10:24:31 -0400

    test: Working with pug to create templates and edit layouts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const calvin = { name: "Calvin", age: 32, cool: true };
+  res.render("hello");
+});
+
+router.get("/reverse/:name", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;

468c3a49bd2462f5442f63afcbae6a4dea916624
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-09-25 16:49:13 -0400

    feat: Test error handling for bad route names

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,8 @@
 exports.myMiddleWare = (req, res, next) => {
   req.name = "Calvin";
+  if (req.name === "Calvin") {
+    throw Error("Wonderful name");
+  }
   next();
 };
 exports.homePage = (req, res) => {

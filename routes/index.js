c03faec2c5a56413d77dd964f2cf17291f9ea4f7
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-09-25 16:21:02 -0400

    test: Test index.pug is being passed

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const storeController = require("../controllers/storeController");
 
-// Do work here
-router.get("/", (req, res) => {
-  const calvin = { name: "Calvin", age: 32, cool: true };
-  res.render("hello", {
-    name: "Calvin",
-    dog: req.query.dog,
-    title: "I love games"
-  });
-});
-
-router.get("/reverse/:name", (req, res) => {
-  const reverse = [...req.params.name].reverse().join("");
-  res.send(reverse);
-});
+router.get("/", storeController.homePage);
 
 module.exports = router;

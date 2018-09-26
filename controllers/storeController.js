6c9d7c83c141a4cb4fde8a6dced79380af5d6e3d
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-09-26 12:24:35 -0400

    feat: Remove middleware  controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,3 @@
-exports.myMiddleWare = (req, res, next) => {
-  req.name = "Calvin";
-  if (req.name === "Calvin") {
-    throw Error("Wonderful name");
-  }
-  next();
-};
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render("index");

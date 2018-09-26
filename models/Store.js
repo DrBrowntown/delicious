2de8f5ea68e275802ab320d2df69a792fea67fca
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-09-26 12:32:49 -0400

    feat: Define mongoose Schema within storeSchema

--- models/Store.js
+++ models/Store.js
@@ -2,4 +2,18 @@ const mongooose = require("mongoose");
 mongooose.Promise = global.Promise;
 const slug = require("slugs");
 
-const storeSchema = new mongooose.Schema({});
+const storeSchema = new mongooose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter a store name"
+  },
+  slug: String,
+  description: {
+    type: string,
+    trim: true
+  },
+  tags: [String]
+});
+
+module.exports = mongoose.model("Store", storeSchema);

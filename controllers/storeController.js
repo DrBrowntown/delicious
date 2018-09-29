3131036d9bbe355eb56fba58be442d28cdbaa67b
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-09-28 22:10:47 -0400

    feat: Add controller and routes for store pages

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,3 +19,7 @@ exports.createStore = async (req, res) => {
   );
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = (req, res) => {
+  res.render("stores", { title: "Stores" });
+};

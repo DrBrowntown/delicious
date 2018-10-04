146469a1172ed00d3f837992024f36dbbe51fa75
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-04 16:31:40 -0400

    bug: Extra space added when altering store slug url

--- models/Store.js
+++ models/Store.js
@@ -47,7 +47,7 @@ storeSchema.pre("save", async function(next) {
   const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, "i");
   const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
   if (storesWithSlug.length) {
-    this.slug = `${this.slug}- ${storesWithSlug.length + 1}`;
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
   }
 
   next();

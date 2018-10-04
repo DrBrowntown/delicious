b38fe1dc0443512a7ad1c2ab639d26aca2342990
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-04 16:04:33 -0400

    feat: Pass store data from controller to store.pug view

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -91,4 +91,5 @@ exports.updateStore = async (req, res) => {
 exports.getStoreBySlug = async (req, res, next) => {
   const store = await Store.findOne({ slug: req.params.slug });
   if (!store) return next();
+  res.render("store", { store, title: store.name });
 };

59285108727efff6d7314a24c3ced511eccbcc53
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-06 15:51:59 -0400

    feat: Flash error message and return user input back to rendered register page

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,16 +8,30 @@ exports.registerForm = (req, res) => {
   res.render("register", { title: "Register" });
 };
 
-exports.validateRegister = (req, res, next) {
-    req.sanitizeBody('name');
-    req.checkBody('name', 'You must supplya  name').notEmpty();
-    req.checkBody('email', 'That email is not valid').isEmail();
-    req.sanitizeBody('email').normalizeEmail({
-        remove_dots: false,
-        remove_extension: false,
-        gmail_remove_subaddress: false
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody("name");
+  req.checkBody("name", "You must supply a  name").notEmpty();
+  req.checkBody("email", "That email is not valid").isEmail();
+  req.sanitizeBody("email").normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false
+  });
+  req.checkBody("password", "Password cannot be blank").notEmpty();
+  req
+    .checkBody("password-confirm", "Confirmed password cannot be blank")
+    .notEmpty();
+  req
+    .checkBody("password-confirm", "Your passwords do not match")
+    .equals(req.body.password);
+
+  const errors = req.validationErrors();
+  if (error) {
+    req.flash("error", errors.map(err => err.msg));
+    res.render("register", {
+      title: "Register",
+      body: req.body,
+      flashes: req.flash()
     });
-    req.checkBody('password', 'Password cannot be blank').notEmpty();
-    req.checkBody('password-confirm', 'Confirmed password cannot be blank').notEmpty();
-    req.checkBody('password-confirm', 'Your passwords do not match').equals(req.body.password);
-}
+  }
+};

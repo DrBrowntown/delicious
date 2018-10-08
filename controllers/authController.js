d9704600f0b94998f167370efef55f7034744b33
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-08 16:57:52 -0400

    feat: Add isLoggedIn middleware check in authController to check if user is authenticated

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,13 @@ exports.logout = (req, res) => {
   req.flash("Success", "You are now logged out");
   res.redirect("/");
 };
+
+exports.isLoggedIn = (req, res, next) => {
+  // check if user is authenticated
+  if (req.isAuthenticated()) {
+    next(); // user is logged in
+    return;
+  }
+  req.flash("error", "You must be logged in to do that");
+  res.redirect("/login");
+};

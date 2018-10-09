1b9883bfb1007146556bcc638a0e3f34c1c0f740
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-08 20:56:46 -0400

    feat: In forgot, check if user email exists

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,6 +25,11 @@ exports.isLoggedIn = (req, res, next) => {
 
 exports.forgot = async (req, res) => {
   // 1. See if a user with that email exists
+  const user = await user.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash("error", "A password reset has been sent to your email");
+    return res.redirect("/login");
+  }
   // 2. Set reset tokens and expiry on their account
   // 3. Send them an email with the token
   // 4. Redirect to login page

280fd162692202efce94eefd8f279a37380c309a
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-09 14:55:25 -0400

    feat: Connect generateHTML method to send method in mail handler

--- handlers/mail.js
+++ handlers/mail.js
@@ -23,11 +23,13 @@ const generateHTML = (filename, options = {}) => {
 };
 
 exports.send = async options => {
+  const html = generateHTML(options.filename, options);
+
   const mailOptions = {
     from: `Calvin Brownlee <calvinbrownlee@gmail.com>`,
     to: options.user.email,
     subject: options.subject,
-    html: "This will be filled later",
+    html: html,
     text: "This will be filled later as well"
   };
   const sendMail = promisify(transport.sendMail, transport);

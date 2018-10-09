aa1fcb8a39c9f209fb23e46ccb7acd1c0c9781c1
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-09 14:45:45 -0400

    feat: Add send method to mail handler and promisify its output

--- handlers/mail.js
+++ handlers/mail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 const pug = require("pug");
 const juice = require("juice");
 const htmlToText = require("html-to-text");
-const promisify = require("promisify");
+const promisify = require("es6-promisify");
 
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -12,3 +12,15 @@ const transport = nodemailer.createTransport({
     pass: process.env.MAIL_PASS
   }
 });
+
+exports.send = async options => {
+  const mailOptions = {
+    from: `Calvin Brownlee <calvinbrownlee@gmail.com>`,
+    to: options.user.email,
+    subject: options.subject,
+    html: "This will be filled later",
+    text: "This will be filled later as well"
+  };
+  const sendMail = promisify(transport.sendMail, transport);
+  return sendMail(mailOptions);
+};

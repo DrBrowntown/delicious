15701c9b4c30a7e7e6cdc5f1d206be825c547e50
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-09 14:36:53 -0400

    feat: Add transport ot mail.js handler

--- handlers/mail.js
+++ handlers/mail.js
@@ -3,3 +3,12 @@ const pug = require("pug");
 const juice = require("juice");
 const htmlToText = require("html-to-text");
 const promisify = require("promisify");
+
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});

3a407b33d802094df3cbc800c1725da99b7f842b
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-08 22:08:56 -0400

    feat: Add update method of authController using user and if statement from reset to check for valid user token

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,3 +70,15 @@ exports.confirmedPasswords = (req, res, next) => {
   req.flash("error", "Passwords do not match");
   res.redirect("back");
 };
+
+exports.update = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+
+  if (!user) {
+    req.flash("error", "Password reset is invalid or has expired");
+    return res.redirect("/login");
+  }
+};

e17384a342dff89128947d46158bee9d0076f633
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-08 20:52:34 -0400

    feat: Add forgot to authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,3 +22,10 @@ exports.isLoggedIn = (req, res, next) => {
   req.flash("error", "You must be logged in to do that");
   res.redirect("/login");
 };
+
+exports.forgot = async (req, res) => {
+  // 1. See if a user with that email exists
+  // 2. Set reset tokens and expiry on their account
+  // 3. Send them an email with the token
+  // 4. Redirect to login page
+};

6b2542bb0395271ab3f04b643ee3ea0dafd31482
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-08 21:06:31 -0400

    feat: Add reset url for password reset token

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,6 +36,15 @@ exports.forgot = async (req, res) => {
   // 2. Set reset tokens and expiry on their account
   user.resetPasswordToken = crypto.randomBytes(20).toString("hex");
   user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
   // 3. Send them an email with the token
+  const resetURL = `http://#{req.headers.host}.account/reset/${
+    user.resetPasswordToken
+  }`;
+  req.flash(
+    "success",
+    `You have been emailed a password reset link. ${resetURL}`
+  );
   // 4. Redirect to login page
+  res.redirect("/login");
 };

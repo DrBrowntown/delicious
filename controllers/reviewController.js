c7bda904700a1fd0f300f423bb1cb34ab6706f2b
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-11 21:13:37 -0400

    feat: Pass user/store parameters and save as new review, flash success message, and redirect them back

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,4 +1,11 @@
-const mongoose = required("mongoose");
+const mongoose = require("mongoose");
 const Review = mongoose.model("Review");
 
-exports.addReview = async (req, res) => {};
+exports.addReview = async (req, res) => {
+  req.body.author = req.user._id;
+  req.body.store = req.params.id;
+  const newReview = new Review(req.body);
+  await newReview.save();
+  req.flash("success", "Review Saved.");
+  res.redirect("back");
+};

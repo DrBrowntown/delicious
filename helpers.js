53c00e90334550f61e74b7f50c4c759c57c7f841
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-12 14:00:41 -0400

    feat: Rename site title

--- helpers.js
+++ helpers.js
@@ -21,7 +21,7 @@ exports.staticMap = ([lng, lat]) =>
 exports.icon = name => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-exports.siteName = `Brown Games`;
+exports.siteName = `Delicious`;
 
 exports.menu = [
   { slug: "/stores", title: "Stores", icon: "store" },
